0a08fd4473599c22679ca676ba4f23626f00f16b
Austin-Deccentric <caugust19.ac@gmail.com>
2022-03-26 23:22:02 +0100

    feature: implemented the token staking contract

--- eth/erc-tokens/staking-dapp/contracts/TokenStake.sol
+++ eth/erc-tokens/staking-dapp/contracts/TokenStake.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "./Token.sol";
+// import "@openzeppelin/contracts/utils/Context.sol";
 
-contract tokenStake is ERC20, ERC20Burnable {
+contract tokenStake {
+
+    Atadia _token;
     // Staker info
     struct Staker {
         // The deposited tokens of the Staker
@@ -16,24 +20,25 @@ contract tokenStake is ERC20, ERC20Burnable {
         uint256 unclaimedRewards;
     }
 
-    unit private _totalStaked; 
+    uint256 private _totalStaked; 
     // Rewards per hour. A fraction calculated as x/10.000.000 to get the percentage
     uint256 public rewardsPerHour = 285; // 0.00285%/h or 25% APR
 
     // Mapping of address to Staker info
     mapping(address => Staker) internal stakers;
 
-    constructor(string memory _name, string memory _symbol)
-        ERC20(_name, _symbol)
-    {}
+    constructor(address contractAddress){
+        _token = Atadia(contractAddress);
+    }
 
     event Staked(address indexed user, uint256 amount);
     event Unstaked(address indexed user, uint256 amount);
     event Rewarded(address indexed user, uint256 reward);
 
     function stake(uint256 _amount) public {
+        require(_token.balanceOf(msg.sender) > 0, "Buy some ATA first.");
         require(
-            balanceOf(msg.sender) >= _amount,
+            _token.balanceOf(msg.sender) >= _amount,
             "Can't stake more than you own"
         );
         if (stakers[msg.sender].deposited == 0) {
@@ -47,7 +52,9 @@ contract tokenStake is ERC20, ERC20Burnable {
             stakers[msg.sender].timeOfLastUpdate = block.timestamp;
         }
 
-        transferFrom(msg.sender, address(this), _amount);
+        _totalStaked += _amount;
+
+        _token.transferFrom(msg.sender, address(this), _amount);
         emit Staked(msg.sender, _amount);
     }
 
@@ -61,32 +68,34 @@ contract tokenStake is ERC20, ERC20Burnable {
             3600) * stakers[_staker].deposited) * rewardsPerHour) / 10000000);
     }
 
-    function claimRewards() external {
+    function claimRewards() public {
         uint256 _rewards = calculateRewards(msg.sender) + stakers[msg.sender].unclaimedRewards;
         require(_rewards > 0, "No rewards!");
         stakers[msg.sender].unclaimedRewards = 0;
         stakers[msg.sender].timeOfLastUpdate = block.timestamp;
-        _mint(msg.sender, _reward);
+        require(_token.issueToken(msg.sender, _rewards), "Unable to claim rewards");
 
-        emit Rewarded(msg.sender, reward);
+        emit Rewarded(msg.sender, _rewards);
         }
-    }
+    
 
     // Withdraw all stake and rewards and mints them to the msg.sender
     function unstake() public {
         require(stakers[msg.sender].deposited > 0, "You have no deposit");
         
-        // uint256 _rewards = calculateRewards(msg.sender) +
-        //     stakers[msg.sender].unclaimedRewards;
         uint256 _deposit = stakers[msg.sender].deposited;
         stakers[msg.sender].deposited = 0;
         stakers[msg.sender].timeOfLastUpdate = 0;
         _totalStaked -= _deposit;
         
-        transfer(msg.sender, _amount);
-        claimRewards();
-
-        emit Unstaked(msg.sender, _amount);
+        _token.transfer(msg.sender, _deposit);
+        uint256 _rewards = calculateRewards(msg.sender) +
+            stakers[msg.sender].unclaimedRewards;
+        
+        if(_rewards > 0) {
+            claimRewards();
+        }
+        emit Unstaked(msg.sender, _deposit);
     }
 
     
@@ -104,5 +113,8 @@ contract tokenStake is ERC20, ERC20Burnable {
         return (_stake, _rewards);
     }
 
+    function totalStaked() external view returns (uint) {
+    return _totalStaked;
+    }
 }
 
